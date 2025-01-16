86a07e2b6e3939c61e4c2fd0aadded13abbb9567
Kiran5655 <142571315+Kiran5655@users.noreply.github.com>
2025-01-16 23:46:10 +0530

    CORS config setup 9

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -6,7 +6,9 @@ export const getJobs = async (req, res) => {
     try {
         const jobs = await Job.find({visible: true})
             .populate({path: "companyId", select: '-password'})
-
+        res.header('Access-Control-Allow-Origin', 'https://job-portal-client-ten-wheat.vercel.app'); // Crucial line
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
         res.json({success: true, jobs})
     } catch (err) {
         res.json({success: false, message: err.message})
