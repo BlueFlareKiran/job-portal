0d8e5a4aa78d7b8f31d87ebaba86b79c3044cff4
Kiran5655 <142571315+Kiran5655@users.noreply.github.com>
2025-01-17 11:04:36 +0530

    CORS config setup 11

--- server/server.js
+++ server/server.js
@@ -20,7 +20,12 @@ await connectCloudinary()
 
 // Middleware
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://job-portal-client-ten-wheat.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
 app.use(express.json())
 app.use(clerkMiddleware())
 
